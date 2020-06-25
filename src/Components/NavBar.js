5968741b8e27e63b560942bf87b8ed6e97599db9
victorvega <victor.vega@protecso.com.pe>
2020-06-25 16:49:07 -0500

    ok

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import Logo from '../Images/logo_512x512.png';
 import logo from '../Images/logo-black.svg';
 
 const Navbar = props => {
@@ -7,21 +6,21 @@ const Navbar = props => {
 
   const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
-  return (   
+  return (
     <nav className="py-3 navbar navbar-expand-lg fixed-top auto-hiding-navbar navbar-light">
-    <div className="container">
-      <a className="navbar-brand py-0" href="/html/index.html" target="_blank"><span style={{ backgroundImage: `url(${logo})` }}></span></a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="navbar-content" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbar-content">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item mx-1"><a className="nav-link" href="/html/index.html">Adopciones</a></li>
-          <li className="nav-item mx-1"><a className="nav-link" href="/html/index.html">Padrinos</a></li>
-          <li className="nav-item mx-1"><a className="nav-link" href="/html/index.html">Dona Comida</a></li>
-          <li className="nav-item mx-1"><a className="nav-link" href="/html/index.html">Valutario</a></li>
-          <li className="nav-item mx-1"><a className="nav-link" href="/html/index.html">Dona Esterilizacion</a></li>
-          {/* <li className="nav-item mx-1 dropdown">
+      <div className="container">
+        <a className="navbar-brand py-0" href="/html/index.html" target="_blank"><span style={{ backgroundImage: `url(${logo})` }}></span></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="navbar-content" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbar-content">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item mx-1"><a className="nav-link" href="/html/index.html">Adopciones</a></li>
+            <li className="nav-item mx-1"><a className="nav-link" href="/html/index.html">Padrinos</a></li>
+            <li className="nav-item mx-1"><a className="nav-link" href="/html/index.html">Dona Comida</a></li>
+            <li className="nav-item mx-1"><a className="nav-link" href="/html/index.html">Valutario</a></li>
+            <li className="nav-item mx-1"><a className="nav-link" href="/html/index.html">Dona Esterilizacion</a></li>
+            {/* <li className="nav-item mx-1 dropdown">
             <a className="nav-link dropdown-toggle" id="demos-dropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Demos</a>
             <div className="dropdown-menu animated_dropdown shadow-lg" aria-labelledby="demos-dropdown"><a className="dropdown-item" href="/html/demo/demo-basic.html">Basic</a><a className="dropdown-item" href="/html/demo/demo-startup1.html">Startup 1</a><a className="dropdown-item" href="/html/demo/demo-startup2.html">Startup 2</a><a className="dropdown-item" href="/html/demo/demo-startup3.html">Startup 3</a><a className="dropdown-item" href="/html/demo/demo-saas1.html">SaaS 1</a><a className="dropdown-item" href="/html/demo/demo-saas2.html">SaaS 2</a><a className="dropdown-item" href="/html/demo/demo-mobile.html">Mobile</a><a className="dropdown-item" href="/html/demo/demo-meetup.html">Meetup</a></div>
           </li>
@@ -39,11 +38,12 @@ const Navbar = props => {
           </li>
           <li className="nav-item mx-2 pt-1"><a className="rounded-pill px-3 btn btn-sm btn-dark" id="buy-button" href="https://themes.getbootstrap.com/product/replacer-2/">Buy</a></li>
           <li className="nav-item mx-1"><button className="nav-link btn btn-link" type="button" data-toggle="modal" data-target="#login-modal"><i className="far fa-user mr-2"></i>Login</button></li> */}
-        </ul>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   );
 }
 
 export default Navbar;
+//https://getreplacer.com/html/demo/demo-basic.html
